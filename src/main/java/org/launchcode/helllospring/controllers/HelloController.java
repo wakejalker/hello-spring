a1d986ab1ccb8b4110b1a147489de04cfdf82282
jake <32205542+wakejalker@users.noreply.github.com>
2024-04-29 17:20:30 -0500

    Chapter 10 Exercises

--- src/main/java/org/launchcode/helllospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/helllospring/controllers/HelloController.java
@@ -11,32 +11,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("hello")
 public class HelloController {
 
-    // Handles request at path /hello/hello
+    //handle request at path http://localhost:8080/hello
 //    @GetMapping("hello")
 //    @ResponseBody
 //    public String hello() {
 //        return "Hello, Spring!";
 //    }
 
-    // Handles request at path /hello/goodbye
+    //handle request at path //http://localhost:8080/hello/goodbye
     @GetMapping("goodbye")
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    // Handles request of the form /hello?name=LaunchCode
+    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
-    // Handles requests of the form /hello/LaunchCode
+    //handle requests of the form http://localhost:8080/hello/LaunchCode
     @GetMapping("hello/{name}")
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
-    // /hello/form
+    //handles requests for http://localhost:8080/hello/form
     @GetMapping("form")
     public String helloForm () {
         return "<html>" +
@@ -48,4 +48,40 @@ public class HelloController {
                 "</body>" +
                 "<html>";
     }
+
+    @RequestMapping(value="hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null) {
+            name = "World";
+        }
+
+        return createMessage(name, language);
+
+        // For a bonus mission, students can change this response text to look nicer.
+        // This is subjective, but students should be modifying the HTML of the response string.
+    }
+
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        if (l.equals("english")) {
+            greeting = "Hello";
+        }
+        else if (l.equals("french")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("german")) {
+            greeting = "Hallo";
+        }
+
+        return greeting + " " + n;
+    }
+
 }
