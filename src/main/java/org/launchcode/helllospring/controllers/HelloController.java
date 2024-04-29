4400acd96d6372e524b98134818706171df65b1a
jake <32205542+wakejalker@users.noreply.github.com>
2024-04-29 14:31:15 -0500

    Controllers with Parameters

--- src/main/java/org/launchcode/helllospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/helllospring/controllers/HelloController.java
@@ -2,6 +2,7 @@ package org.launchcode.helllospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -10,18 +11,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    // Handles request at /hello
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello, Spring!";
-    }
+    // Handles request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
 
-    // Handles request at /goodbye
+    // Handles request at path /goodbye
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
+    }
+
+    // Handles request of the form /hello?name=LaunchCode
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 
 }
