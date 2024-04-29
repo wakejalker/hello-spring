983699334d8b2fed285f8183628d413e77eed1b3
jake <32205542+wakejalker@users.noreply.github.com>
2024-04-29 15:19:00 -0500

    Class level controller annotations

--- src/main/java/org/launchcode/helllospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/helllospring/controllers/HelloController.java
@@ -7,38 +7,37 @@ import org.springframework.web.bind.annotation.*;
  *  Created by Jake
  */
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
-    // Handles request at path /hello
+    // Handles request at path /hello/hello
 //    @GetMapping("hello")
 //    @ResponseBody
 //    public String hello() {
 //        return "Hello, Spring!";
 //    }
 
-    // Handles request at path /goodbye
+    // Handles request at path /hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
     // Handles request of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
-    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
     // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
+    // /hello/form
     @GetMapping("form")
-    @ResponseBody
     public String helloForm () {
         return "<html>" +
                 "<body>" +
