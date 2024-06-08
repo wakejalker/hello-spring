20d83aa8034d4bf6a112589544300482c2397218
jake <32205542+wakejalker@users.noreply.github.com>
2024-06-08 17:09:47 -0500

    Ch10 Exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -7,43 +7,83 @@ import org.springframework.web.bind.annotation.*;
 @ResponseBody
 @RequestMapping("hello")
 public class HelloController {
-    //handle request at path http://localhost:8080/hello
 
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello(){
-//        return "Hello, Spring!";
-//    }
+    // Handle request at path http://localhost:8080/hello
+    @GetMapping
+    public String hello(){
+        return "Hello, Spring!";
+    }
 
-    //handle request at path //http://localhost:8080/hello/goodbye
+    // Handle request at path http://localhost:8080/hello/goodbye
     @GetMapping("goodbye")
     public String goodbye(){
         return "Goodbye, Spring!";
     }
 
-    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name){
+    // Handle requests of the form http://localhost:8080/hello/greet?name=LaunchCode
+    @RequestMapping(value = "greet", method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam(required = false) String name){
+        if (name == null || name.isEmpty()) {
+            return "Hello, Spring!";
+        }
         return "Hello, " + name + "!";
     }
 
-    //handle requests of the form http://localhost:8080/hello/LaunchCode
+    // Handle requests of the form http://localhost:8080/hello/LaunchCode
     @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
-    //handles requests for http://localhost:8080/hello/form
+    // Handle requests for http://localhost:8080/hello/form
     @GetMapping("form")
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
-                "<input type = 'text' name = 'name' >" +
-                "<input type = 'submit' value = 'Greet Me!' >" +
+                "<form action='/hello' method='post'>" + // submit a request to /hello
+                "<label for='name'>Name:</label>" +
+                "<input type='text' name='name'>" +
+                "<label for='language'>Language:</label>" +
+                "<select name='language'>" +
+                "<option value='english'>English</option>" +
+                "<option value='french'>French</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='german'>German</option>" +
+                "<option value='chinese'>Chinese</option>" +
+                "</select>" +
+                "<input type='submit' value='Greet Me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
 
+    // Handle the form submission
+    @PostMapping
+    public String helloWithForm(@RequestParam String name, @RequestParam String language) {
+        String greeting = createGreeting(name, language);
+        return greeting;
+    }
+
+    // Method to create greeting based on language
+    private String createGreeting(String name, String language) {
+        String greeting;
+        switch (language.toLowerCase()) {
+            case "french":
+                greeting = "Bonjour, " + name + "!";
+                break;
+            case "spanish":
+                greeting = "Hola, " + name + "!";
+                break;
+            case "german":
+                greeting = "Hallo, " + name + "!";
+                break;
+            case "chinese":
+                greeting = "Nǐ hǎo, " + name + "!";
+                break;
+            default:
+                greeting = "Hello, " + name + "!";
+                break;
+        }
+        return greeting;
+    }
 }
